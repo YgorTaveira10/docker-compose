112bb7872bd942b7c56b6758244fd608f9861fbc
Ygor Taveira <89111908+YgorTaveira10@users.noreply.github.com>
2024-10-02 01:08:42 +0000

    commitfinal

--- rush.sh
+++ rush.sh
@@ -1,9 +1,18 @@
 
-     docker-compose up -d
-     docker-compose ps
-     echo "Aguardando a carga do postgres"
-     sleep 5
-     docker-compose exec db psql -U postgres -c '\l'
-     docker-compose down
+docker-compose up -d
+docker-compose ps
+echo "Aguardando a carga do postgres"
+sleep 5
+docker-compose exec db psql -U postgres -c '\l'
+docker-compose down
 
-     docker-compose exec db psql -U postgres -f /scripts/check.sql
+docker-compose exec db psql -U postgres -f /scripts/check.sql
+
+
+docker-compose up -d
+docker-compose logs -f -t
+
+
+docker-compose up -d
+docker-compose scale worker=3 & 
+docker-compose logs -f -t worker app
